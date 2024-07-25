98ea94e57e81e59793e6fcab362cb9bd099a235e
Rifat Mahmud <mahmudrifat764@gmail.com>
2024-07-26 03:43:09 +0600

    backend complete

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ProductRoutes } from '../modules/Products/product.route';
+import { OrderRoutes } from '../modules/Orders/order.route';
 
 const router = Router();
 
@@ -8,6 +9,10 @@ const moduleRoutes = [
         path: '/product',
         route: ProductRoutes,
     },
+    {
+        path: '/order',
+        route: OrderRoutes
+    }
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
